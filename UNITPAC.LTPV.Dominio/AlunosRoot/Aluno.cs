874ba85b3ee0e4aede7245a723cde4fdff0028f2
PedroHenriqueSI <57466217+PedroHenriqueSI@users.noreply.github.com>
2020-10-23 17:25:28 -0300

    Iniciando

---
+++ UNITPAC.LTPV.Dominio/AlunosRoot/Aluno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNITPAC.LTPV.Dominio.AlunosRoot
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SobreNome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Matricula { get; set; }
+    }
+}
